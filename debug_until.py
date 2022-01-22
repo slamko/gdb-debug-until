9dbb93da03cc79bbf3fcea516afd11ee37f25382
Viacelaus <vaceslavkozin619@gmail.com>
2022-01-22 13:07:08 +0200

    fix disconnection from events

--- debug_until.py
+++ debug_until.py
@@ -1,8 +1,13 @@
 from enum import Enum
+import gdb
 
 cmd = ''
 exp = ''
 start_point = None
+triggered = 0
+
+LOG_FILE = 'log.txt'
+GDB_LOG = 'gdblog.txt'
 
 def get_arg_value(args):
     return args.split('=')[1]
@@ -19,35 +24,39 @@ class EX_CODE(Enum):
 
 
 def finish_debug():
-	gdb.events.stop.disconnect(check_st)
-	gdb.events.exited.disconnect(check_st)
+	global handler
+	gdb.events.stop.disconnect(handler)
+	gdb.events.exited.disconnect(handler)
 	global start_point
 	start_point.delete()
-	gdb.execute("shell rm log.txt")
+	gdb.execute('shell rm {0}'.format(LOG_FILE))
+	gdb.execute('set logging off')
 
 
 def run():
 	global cmd 
 	global exp
-	shell_cmd = '{0} > log.txt 2>&1'.format(cmd)
+	shell_cmd = '{0} > {1} 2>&1'.format(cmd, LOG_FILE)
 	gdb.execute(shell_cmd, from_tty=True, to_string=True)
-	res = open("log.txt").read()
+	res = open(LOG_FILE).read()
 
 	if exp == res or res.startswith(exp):
-		gdb.write(' \n')
-		gdb.write('-------------------\n')
-		gdb.write('Event triggered!\n')
-		gdb.write('-------------------\n')
-		gdb.write(' \n')
-		gdb.execute('continue')
-		finish_debug()
+		global triggered
+		if triggered == 0: 
+			gdb.write(' \n')
+			gdb.write('----------------\n')
+			gdb.write('Event triggered!\n')
+			gdb.write('----------------\n')
+			gdb.write(' \n')
+			triggered = 1
+			finish_debug()
+			gdb.execute('next')
+			gdb.execute('continue')
 	else:
 		gdb.execute('next')
 
 
 def check_st(event):
-		global cmd
-		global exp
 		if hasattr (event, 'breakpoints'):
 			run()
 		elif hasattr (event, 'inferior'):
@@ -55,6 +64,7 @@ def check_st(event):
 		else:
 			run()
 	
+handler = check_st
 
 def start_debug(args):
 	if '--cmp' in args[2] and '--exp' in args[3]:
@@ -62,8 +72,19 @@ def start_debug(args):
 		cmd = get_arg_value(args[2])
 		global exp 
 		exp = get_arg_value(args[3])
-		gdb.events.stop.connect(check_st)
-		gdb.events.exited.connect(check_st)
+
+		shell_cmd = '{0} > {1} 2>&1'.format(cmd, LOG_FILE)
+		gdb.execute(shell_cmd, from_tty=True, to_string=True)
+		res = open(LOG_FILE).read()
+		if res == exp or res.startswith(exp):
+			gdb.write(' \n')
+			gdb.write('Condition is already satisfied\n')
+			return DB_STATUS.COMPLETED
+		
+		global handler
+		gdb.events.stop.connect(handler)
+		gdb.events.exited.connect(handler)
+		return DB_STATUS.RUNNING
 
 
 class DebugUntil (gdb.Command):
@@ -77,11 +98,16 @@ class DebugUntil (gdb.Command):
 		if not  arg:
 			return
 
+		gdb.execute('set logging file {0}'.format(GDB_LOG))
+		gdb.execute('set logging on')
+
 		args = gdb.string_to_argv(arg)
 		global start_point
 		start_point = gdb.Breakpoint(args[0])
-			
-		start_debug(args)
+
+		db_st = start_debug(args)
+		if db_st == DB_STATUS.COMPLETED:
+			return
 		
 		if '--args' not in arg:
 			gdb.execute('run')

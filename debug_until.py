bd78660aa96187ab15a640341a5ce4df9d5e4624
Viacelaus <vaceslavkozin619@gmail.com>
2022-01-23 00:31:22 +0200

    fix behaviour on untriggered event

--- debug_until.py
+++ debug_until.py
@@ -7,7 +7,12 @@ start_point = None
 triggered = 0
 
 LOG_FILE = 'log.txt'
-GDB_LOG = 'gdblog.txt'
+CMD = 'debug-until'
+
+NEXT = 'next'
+CONTINUE = 'continue'
+RUN = 'run'
+
 
 def get_arg_value(args):
     return args.split('=')[1]
@@ -23,6 +28,33 @@ class EX_CODE(Enum):
     FAIL = 1
 
 
+def cmp_output(res):
+	global exp
+	return exp == res or res.startswith(exp)
+
+
+def event_triggered():
+	gdb.write('\n----------------\n')
+	gdb.write('Event triggered!\n')
+	gdb.write('----------------\n \n')
+
+
+def report_debug_failure():
+	gdb.write('\n----------------\n')
+	gdb.write('Passed condition wasn`t met. Try setting defferent parameters\n')
+	gdb.write('----------------\n \n')
+
+
+def run_shell_command():
+	shell_cmd = '{0} > {1} 2>&1'.format(cmd, LOG_FILE)
+	gdb.execute(shell_cmd, from_tty=True, to_string=True)
+	return open(LOG_FILE).read()
+
+
+def condition_satisfied():
+	gdb.write('\nCondition is already satisfied\n')
+
+
 def finish_debug():
 	global handler
 	gdb.events.stop.disconnect(handler)
@@ -30,37 +62,40 @@ def finish_debug():
 	global start_point
 	start_point.delete()
 	gdb.execute('shell rm {0}'.format(LOG_FILE))
-	gdb.execute('set logging off')
+
+
+def check_trig_event():
+	global triggered
+	if triggered == 0: 
+		event_triggered()
+		triggered = 1
+		finish_debug()
+		gdb.execute(NEXT)
+		gdb.execute(CONTINUE)
 
 
 def run():
 	global cmd 
-	global exp
-	shell_cmd = '{0} > {1} 2>&1'.format(cmd, LOG_FILE)
-	gdb.execute(shell_cmd, from_tty=True, to_string=True)
-	res = open(LOG_FILE).read()
+	res = run_shell_command()
 
-	if exp == res or res.startswith(exp):
-		global triggered
-		if triggered == 0: 
-			gdb.write(' \n')
-			gdb.write('----------------\n')
-			gdb.write('Event triggered!\n')
-			gdb.write('----------------\n')
-			gdb.write(' \n')
-			triggered = 1
-			finish_debug()
-			gdb.execute('next')
-			gdb.execute('continue')
+	if cmp_output(res):
+		check_trig_event()
 	else:
-		gdb.execute('next')
+		gdb.execute(NEXT)
 
 
 def check_st(event):
 		if hasattr (event, 'breakpoints'):
 			run()
 		elif hasattr (event, 'inferior'):
-			finish_debug()
+			global cmd 
+			res = run_shell_command()
+
+			if cmp_output(res):
+				check_trig_event()
+			else:
+				report_debug_failure()
+				finish_debug()
 		else:
 			run()
 	
@@ -73,12 +108,9 @@ def start_debug(args):
 		global exp 
 		exp = get_arg_value(args[3])
 
-		shell_cmd = '{0} > {1} 2>&1'.format(cmd, LOG_FILE)
-		gdb.execute(shell_cmd, from_tty=True, to_string=True)
-		res = open(LOG_FILE).read()
-		if res == exp or res.startswith(exp):
-			gdb.write(' \n')
-			gdb.write('Condition is already satisfied\n')
+		res = run_shell_command()
+		if cmp_output(res):
+			condition_satisfied()
 			return DB_STATUS.COMPLETED
 		
 		global handler
@@ -91,15 +123,12 @@ class DebugUntil (gdb.Command):
 	"""Debug through the function until the passed condition would be met"""
 
 	def __init__(self):
-		super(DebugUntil, self).__init__("debug-until", gdb.COMMAND_USER)
+		super(DebugUntil, self).__init__(CMD, gdb.COMMAND_USER)
 
 
 	def invoke(self, arg, from_tty):
 		if not  arg:
 			return
-
-		gdb.execute('set logging file {0}'.format(GDB_LOG))
-		gdb.execute('set logging on')
 
 		args = gdb.string_to_argv(arg)
 		global start_point
@@ -110,9 +139,9 @@ class DebugUntil (gdb.Command):
 			return
 		
 		if '--args' not in arg:
-			gdb.execute('run')
+			gdb.execute(RUN)
 		else:
 			p_args = get_arg_value(args[1])
-			gdb.execute('run {0}'.format(p_args))
+			gdb.execute('{0} {1}'.format(RUN, p_args))
 
 DebugUntil()
